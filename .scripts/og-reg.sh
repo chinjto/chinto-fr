2093ee7373a503c74c8ca85247edc53f8a45f266
chinjto <contact@cyril-defaye.fr>
2026-06-25 17:42:15 +0100

    wip(no-reg): OG logs OK
    - generated OG logs iso previous OG logs
    - generated OG logs iso previous prerendered OG logs
    
    Closes #10

--- .scripts/og-reg.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-echo "OG no-regression tests starting..."
-npm run og -- http://localhost:8080/japon > .log/render-og-japan.log
-npm run og -- http://localhost:8080 > .log/render-og-home.log
-npm run og -- http://localhost:8080/voyages > .log/render-og-travel.log
-npm run og -- http://localhost:8080/jeux-de-societe > .log/render-og-board-games.log
-echo "OG no-regression tests finished."
-echo "See logs .log/render-og*.log for more details."
