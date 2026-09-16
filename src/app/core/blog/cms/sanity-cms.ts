bda70297cd716f6bb93428f2f638367ee61356de
chinjto <contact@cyril-defaye.fr>
2026-09-16 21:41:16 +0200

    feat(blog): Implement sanity CMS service
    - Add sanity client configuration
    - Add articles list feature

--- src/app/core/blog/cms/sanity-cms.ts
+++ src/app/core/blog/cms/sanity-cms.ts
@@ -1,28 +1,53 @@
 import {Injectable, Signal, signal} from '@angular/core';
 import {Cms} from '@core/blog/cms/cms';
 import { Article } from "../article";
+import {createClient} from '@sanity/client';
+
+// TODO à bousculer dans des variables de déploiement
+// rien de sensible car publique (peut partir sur Github), mais plus propre de le variabiliser par env'
+export const sanityClient = createClient({
+  projectId: 'msozvpkg',
+  dataset: 'production',
+  apiVersion: '2026-09-16',
+  useCdn: true
+});
+
+interface SanityArticle {
+  slug: string;
+  title: string;
+  summary: string;
+  publishedAt: string;
+}
+
+function _toArticle(article: SanityArticle): Article {
+  return {
+    slug: article.slug,
+    title: article.title,
+    summary: article.summary,
+    publishedAt: new Date(article.publishedAt),
+  };
+}
 
 @Injectable({providedIn: "root"})
 export class SanityCms implements Cms {
 
-  private _mockedArticles = signal<Article[]>([
-    {
-      slug: 'aaa',
-      title: 'AAA',
-      summary: 'This is a first article.',
-      publishedAt: new Date('2026-09-16T16:00:00')
-    },
-    {
-      slug: 'bbb',
-      title: 'BBB',
-      summary: 'This is a second article.',
-      publishedAt: new Date('2026-09-16T17:00:00')
-    }
-  ]);
+  private readonly _articles = signal<Article[]>([]);
 
   articles(): Signal<Article[]> {
-      return this._mockedArticles.asReadonly();
+      sanityClient.fetch(`
+          *[_type == "article"] | order(publishedAt desc) {
+            "slug": slug.current,
+            title,
+            summary,
+            publishedAt
+          }
+        `)
+        .then(articles => {
+          this._articles.set(articles.map(_toArticle));
+        });
+      return this._articles.asReadonly();
   }
+
   article(slug: string): Signal<Article> {
       throw new Error("Method not implemented.");
   }
